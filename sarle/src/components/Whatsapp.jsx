2906a318c6d63369200bfd816fcb95050b27500f
Z4ffarani <zaffarani.kaique@gmail.com>
2025-01-07 00:36:13 -0300

    sarle number

--- sarle/src/components/Whatsapp.jsx
+++ sarle/src/components/Whatsapp.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from 'react-icons/fa'
 
 export default function Whatsapp() {
     return (
-        <a href="https://api.whatsapp.com/send?phone=5511973786529" target="_blank" rel="noreferrer">
+        <a href="https://api.whatsapp.com/send?phone=5511975440070" target="_blank" rel="noreferrer">
             <div className='z-50 rounded-full p-3 bg-green-500 bg-opacity-[70%] fixed right-7 bottom-16 hover:bg-opacity-100 active:scale-90 transition-all duration-300'>
                 <FaWhatsapp className="text-white text-[45px] cursor-pointer"/>
             </div>
