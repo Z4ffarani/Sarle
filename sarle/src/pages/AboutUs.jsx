5ff15571d34c611b0ab4f534559f8e30a05495ac
Z4ffarani <zaffarani.kaique@gmail.com>
2025-01-17 12:23:41 -0300

    changes required

--- sarle/src/pages/AboutUs.jsx
+++ sarle/src/pages/AboutUs.jsx
@@ -84,7 +84,7 @@ export default function AboutUs() {
                 </div>
             </div>
 
-            <h2 style={{ fontFamily: 'airbus' }} className='pt-10 sm:mb-[-30px] sm:py-16 text-white text-center text-[33px] md:text-[45px] lg:text-[60px] opacity-40 text-wrap px-5 select-none'>COM QUEM TRABALHAMOS</h2>
+            <h2 style={{ fontFamily: 'airbus' }} className='pt-10 sm:mb-[-30px] sm:py-16 text-white text-center text-[33px] md:text-[45px] lg:text-[60px] opacity-40 text-wrap px-5 select-none'>EQUIPE</h2>
 
             <div className="flex justify-center mx-5">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
